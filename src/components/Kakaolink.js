1741839c96e2908c8f58e6b4004f81a845384c66
thecharmingpark <thecharmingprk@gmail.com>
2023-01-16 18:05:40 +0900

    feat: kakao 주소 저장

--- src/components/Kakaolink.js
+++ src/components/Kakaolink.js
@@ -10,13 +10,9 @@ const Kakaolink = () => {
         link: {
           // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
           mobileWebUrl:
-            'https://ever-ever-after-chanmin-n-dongjin-' +
-            `${process.env.REACT_APP_REBUILD_KEY}` +
-            '-charmingpark.vercel.app',
+            'https://ever-ever-after-chanmin-n-dongjin-disfullpage.vercel.app',
           webUrl:
-            'https://ever-ever-after-chanmin-n-dongjin-' +
-            `${process.env.REACT_APP_REBUILD_KEY}` +
-            '-charmingpark.vercel.app',
+            'https://ever-ever-after-chanmin-n-dongjin-disfullpage.vercel.app',
         },
       },
       buttons: [
